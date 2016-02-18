f5a70d177cd5640c7ca1b0666adb412b28d9dd56
jtanigawa <joseph.tanigawa@live.com>
2016-02-17 18:30:02 -0800

    cleaned up spacing

--- Advanced JavaScript/day1/exercises/ex4/ex4-solution-better.js
+++ Advanced JavaScript/day1/exercises/ex4/ex4-solution-better.js
@@ -24,16 +24,16 @@ Button.layout = function (width, height, label) {
   this.$elem = $('<button>').text(this.label)
 }
 
-Button.display = function($where) {
+Button.display = function ($where) {
 	this.render($where)
 	this.$elem.click(this.onClick.bind(this))
 }
 
-Button.onClick = function(evt) {
+Button.onClick = function (evt) {
 	console.log('Button ' + this.label + ' clicked!')
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 	var $body = $(document.body)
 
 	var btn1 = Object.create(Button)
@@ -44,4 +44,3 @@ $(document).ready(function(){
 	btn2.layout(200, 100, 'button #2')
 	btn2.display($body)
 })
-
